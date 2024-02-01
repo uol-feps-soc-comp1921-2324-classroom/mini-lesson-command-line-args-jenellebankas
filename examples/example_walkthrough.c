2069c7b8d21863ca867b5fb8febd84d765cf08bc
jenellebankas <139067450+jenellebankas@users.noreply.github.com>
2024-02-01 10:26:13 +0000

    looking at examples

--- examples/example_walkthrough.c
+++ examples/example_walkthrough.c
@@ -5,18 +5,16 @@
  * argv[1] - name
  * argv[2] - age
  */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    if (argc != 3)
-    {
+    if (argc != 2) {
         printf("Usage: wlk <name>\n");
         return 0;
     }
 
     int age = atoi(argv[2]);
-    if (age < 1)
-    {
+
+    if (age < 1) {
         printf("Error: invalid age\n");
         return 1;
     }
