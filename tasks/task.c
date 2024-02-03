17715fb14f6285aee909105b7761e436049035d9
jenellebankas <139067450+jenellebankas@users.noreply.github.com>
2024-02-03 17:45:51 +0000

    changed error thingy

--- tasks/task.c
+++ tasks/task.c
@@ -13,16 +13,16 @@ int main(int argc, char *argv[]) {
     // check if there are the correct number of arguments 
     if (argc != 3) {
         printf("Usage: %s <number1> <number2>\n", argv[0]);
-        return 0;
+        return 1;
     }
 
     // check if the inputs are integers
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
 
-    if ((num1 < 1) || (num2 < 1)) {
+    if ((num1 < 0) || (num2 < 0)) {
         printf("The input must be an integer.\n");
-        return 0;
+        return 1;
     }
 
     // Calculate and print the sum
